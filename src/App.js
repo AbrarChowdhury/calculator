20e55cfa58619bf747ec138564a1303415344ab7
abrar shams chowdhury <abrar.chowdhury99@gmail.com>
2022-01-14 17:45:39 +0600

    style: create button grid

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-
+import ButtonGrid from "./components/ButtonGrid";
 
 function App() {
   return (
     <div className="App">
       <h1>Calculator</h1>
+      <ButtonGrid/>
     </div>
   );
 }
---
+++ src/components/ButtonGrid.jsx
@@ -0,0 +1,29 @@
+import "./styles.scss"
+
+function ButtonGrid() {
+    return (
+        <div id="buttonGrid">
+            <button id="clear" className="wide">AC</button>
+            <button id="history">H</button>
+            <button id="divide">%</button>
+            <button id="seven">7</button>
+            <button id="eight">8</button>
+            <button id="nine">9</button>
+            <button id="multiply">x</button>
+            <button id="four">4</button>
+            <button id="five">5</button>
+            <button id="six">6</button>
+            <button id="subtract">-</button>
+            <button id="">1</button>
+            <button id="">2</button>
+            <button id="">3</button>
+            <button id="">+</button>
+            <button id="">0</button>
+            <button id="">.</button>
+            <button id="" className="wide">=</button>
+            
+        </div>
+    )
+}
+
+export default ButtonGrid
