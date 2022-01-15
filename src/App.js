efed4f6d041282e5d0bb549e514d8833f53aaf15
abrar shams chowdhury <abrar.chowdhury99@gmail.com>
2022-01-15 23:10:14 +0600

    style: make it beautiful

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import CalculatorShell from "./components/CalculatorShell";
 function App() {
   return (
     <div className="App">
+    <div style={{margin:"auto"}}>
       <CalculatorShell/>
+      <p style={{textAlign:"center", color:"rgba(255,255,255,0.5)"}}>Made with love, by abrar</p>
+    </div>
     </div>
   );
 }
